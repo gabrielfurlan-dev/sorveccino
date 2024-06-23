4325d4c0d1840e8e0590ac3bc65e6dd197a58f65
Gabriel Furlan <gabriel.silva@inovafarma.com.br>
2024-06-23 17:18:39 -0300

    fix: db configs

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,19 +1,9 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-    DATABASE_URL: z.string(),
-    DATABASE_PORT: z.coerce.number(),
-    DATABASE_HOST: z.string(),
-    DATABASE_USER: z.string(),
-    DATABASE_PASSWORD: z.string(),
-    DATABASE_NAME: z.string(),
+    DATABASE_URL: z.string()
 })
 
 export const env = envSchema.parse({
-    DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_HOST: process.env.DATABASE_HOST,
-    DATABASE_USER: process.env.DATABASE_USER,
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-    DATABASE_NAME: process.env.DATABASE_NAME,
-    DATABASE_PORT: process.env.DATABASE_PORT,
+    DATABASE_URL: process.env.DATABASE_URL
 })
