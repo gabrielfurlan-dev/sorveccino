db2be400c8b33b42994d50a19340b5982a42efd9
Gabriel Furlan <gabriel.silva@inovafarma.com.br>
2024-06-24 23:20:50 -0300

    chore: change migrations locale

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './src/db/schemas',
-  out: './src/db',
+  out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL
--- src/db/migrations/0000_burly_sunfire.sql
+++ src/db/migrations/0000_burly_sunfire.sql
@@ -1,18 +1,3 @@
-CREATE TABLE IF NOT EXISTS "acais" (
-	"id" text PRIMARY KEY NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"nome" text NOT NULL,
-	"categoria" text NOT NULL,
-	"tamanho" numeric NOT NULL,
-	"unidade_medida" text NOT NULL,
-	"preco" numeric NOT NULL,
-	"id_embalagem" text,
-	"adicionais" jsonb,
-	"id_promocao" text,
-	"observacoes" text
-);
---> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "adicionais" (
 	"id" text PRIMARY KEY NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
@@ -30,7 +15,7 @@ CREATE TABLE IF NOT EXISTS "clientes" (
 	"cpf" text,
 	"email" text,
 	"telefone" text,
-	"cep" text NOT NULL,
+	"cep" text,
 	"logradouro" text,
 	"complemento" text,
 	"bairro" text,
@@ -53,7 +38,7 @@ CREATE TABLE IF NOT EXISTS "pedidos" (
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"nome_cliente" text NOT NULL,
-	"acais" text NOT NULL,
+	"acais" jsonb NOT NULL,
 	"cupom_desconto" text NOT NULL,
 	"id_cliente" text
 );
@@ -77,18 +62,6 @@ CREATE TABLE IF NOT EXISTS "promocoes" (
 	"dias_da_semana" jsonb NOT NULL,
 	"ativo" boolean NOT NULL
 );
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "acais" ADD CONSTRAINT "acais_id_embalagem_embalagens_id_fk" FOREIGN KEY ("id_embalagem") REFERENCES "public"."embalagens"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "acais" ADD CONSTRAINT "acais_id_promocao_promocoes_id_fk" FOREIGN KEY ("id_promocao") REFERENCES "public"."promocoes"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "embalagens" ADD CONSTRAINT "embalagens_id_item_itens_id_fk" FOREIGN KEY ("id_item") REFERENCES "public"."itens"("id") ON DELETE no action ON UPDATE no action;
