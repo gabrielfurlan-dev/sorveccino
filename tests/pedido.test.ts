6e52b12a79a9cc97268d49fb87f96999498556ee
Gabriel Furlan <gabriel.silva@inovafarma.com.br>
2024-07-06 09:57:36 -0300

    test: add test to update order status

--- tests/pedido.test.ts
+++ tests/pedido.test.ts
@@ -1,8 +1,10 @@
-import { GetOrderUseCase } from "../src/lib/orders/useCases/GetOrdersUseCase";
+import { GetOrdersUseCase } from "../src/lib/orders/useCases/GetOrdersUseCase";
 import { describe, expect } from "bun:test";
 import { AddOrderCommand } from "@/lib/orders/commands/AddOrderCommand";
 import { AddOrderUseCase } from "@/lib/orders/useCases/AddOrderUseCase";
 import { Order } from "@/lib/orders/types/Order";
+import { GetOrderByIdUseCase } from "@/lib/orders/useCases/GetOrderByIdUseCase";
+import { UpdateStatusOrderUseCase } from "@/lib/orders/useCases/UpdateStatusOrderUseCase";
 
 describe("INTEGRATION - Should Save Order", async () => {
   const order: AddOrderCommand = {
@@ -35,15 +37,29 @@ describe("INTEGRATION - Should Save Order", async () => {
     status: "pending",
   };
 
-  const addOrder = new AddOrderUseCase();
-  const result = await addOrder.execute(order);
-  console.log(result);
-  expect(result.success).toBe(true);
+  // const addOrder = new AddOrderUseCase();
+  // const result = await addOrder.execute(order);
+  // console.log(result);
+  // expect(result.success).toBe(true);
 });
 
 describe("INTEGRATION - Should get orders", async () => {
-  const useCase = new GetOrderUseCase();
-  const result = await useCase.execute();
-  console.log(result.data as Order[]);
-  expect(result.success).toBe(true);
+  // const useCase = new GetOrdersUseCase();
+  // const result = await useCase.execute();
+  // console.log(result.data as Order[]);
+  // expect(result.success).toBe(true);
 });
+
+describe("INTEGRATION - Should get order by id", async () => {
+  // const useCase = new GetOrderByIdUseCase();
+  // const result = await useCase.execute("qzg0quwl6ko6vabsqlpymgjw");
+  // console.log(result.data as Order);
+  // expect(result.success).toBe(true);
+})
+
+describe("INTEGRATION - Should update order status", async () => {
+  // const useCase = new UpdateStatusOrderUseCase();
+  // const result = await useCase.execute("qzg0quwl6ko6vabsqlpymgjw", "completed");
+  // console.log(result.message);
+  // expect(result.success).toBe(true);
+})
