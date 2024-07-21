7b378d08f473a16d8256efd9cc5f22a66d40412b
Gabriel Furlan <gabriel.silva@inovafarma.com.br>
2024-07-21 15:07:57 -0300

    chore: front now uses back-end to list orders

--- v2/src/app/order/orders/page.tsx
+++ v2/src/app/order/orders/page.tsx
@@ -1,54 +1,64 @@
+import { OrderUseCases } from "@/lib/Backend/UseCases/OrderUseCases";
 import { Footer } from "./components/footer";
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-
-const orders = [
-    { id: 1, nome: "Cliente A", dataHora: "2024-07-21 14:30", valorTotal: "100,00", valorPagar: "50,00" },
-    { id: 2, nome: "Cliente B", dataHora: "2024-07-21 15:00", valorTotal: "200,00", valorPagar: "100,00" },
-    { id: 3, nome: "Cliente B", dataHora: "2024-07-21 15:00", valorTotal: "200,00", valorPagar: "100,00" },
-    { id: 3, nome: "Cliente B", dataHora: "2024-07-21 15:00", valorTotal: "200,00", valorPagar: "100,00" },
-    { id: 3, nome: "Cliente B", dataHora: "2024-07-21 15:00", valorTotal: "200,00", valorPagar: "100,00" },
-    { id: 3, nome: "Cliente B", dataHora: "2024-07-21 15:00", valorTotal: "200,00", valorPagar: "100,00" },
-];
-
-const formatCurrency = (value: number) => {
-    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-}
-
-const totalAmount = orders.reduce((acc, order) => acc + parseFloat(order.valorPagar.replace(',', '.')), 0);
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { use } from "react";
 
 export default function Order() {
+  const orders = use(OrderUseCases.GetAll());
+  const totalAmount = orders.reduce(
+    (acc, order) => acc + order.totalToRecieve,
+    0
+  );
 
-    return (
-        <div className="flex flex-col w-full h-[100vh]">
-            <div className="px-20 pt-14">
-                <h1>Todos os pedidos</h1>
-                <div className="mt-4 max-h-[70vh] overflow-y-auto px-20">
-                    <Table>
-                        <TableCaption>Lista dos últimos pedidos</TableCaption>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[50px]">ID</TableHead>
-                                <TableHead>Nome do Cliente</TableHead>
-                                <TableHead>Dia/Hora</TableHead>
-                                <TableHead className="text-right">Valor Total</TableHead>
-                                <TableHead className="text-right">Valor a Ser Pago</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {orders.map((order) => (
-                                <TableRow key={order.id}>
-                                    <TableCell className="font-medium">{order.id}</TableCell>
-                                    <TableCell>{order.nome}</TableCell>
-                                    <TableCell>{order.dataHora}</TableCell>
-                                    <TableCell className="text-right">{order.valorTotal}</TableCell>
-                                    <TableCell className="text-right">{order.valorPagar}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>
-            </div>
-            <Footer total={formatCurrency(totalAmount)} />
+  const formatCurrency = (value: number) => {
+    return value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
+
+  return (
+    <div className="flex flex-col w-full h-[100vh]">
+      <div className="px-20 pt-14">
+        <h1>Todos os pedidos</h1>
+        <div className="mt-4 max-h-[70vh] overflow-y-auto px-20">
+          <Table>
+            <TableCaption>Lista dos últimos pedidos</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[50px]">ID</TableHead>
+                <TableHead>Nome do Cliente</TableHead>
+                <TableHead>Dia/Hora</TableHead>
+                <TableHead className="text-right">Valor Total</TableHead>
+                <TableHead className="text-right">Valor a Ser Pago</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell className="font-medium">{order.id}</TableCell>
+                  <TableCell>{order.customer.name}</TableCell>
+                  <TableCell>{order.createdAt.toISOString()}</TableCell>
+                  <TableCell className="text-right">{order.total}</TableCell>
+                  <TableCell className="text-right">
+                    {order.totalToRecieve}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
-    )
+      </div>
+      <Footer total={formatCurrency(totalAmount)} />
+    </div>
+  );
 }
--- v2/src/lib/Backend/UseCases/OrderUseCases.ts
+++ v2/src/lib/Backend/UseCases/OrderUseCases.ts
@@ -1,21 +1,63 @@
-import { Order } from '@/lib/Backend/Types/Order';
+import { Order } from "@/lib/Backend/Types/Order";
 
-let Orders: Order[] = [];
+export class OrderUseCases {
+  private static orders: Order[] = [];
 
-export function Add(order: Order) {
-  Orders.push(order);
-  return order;
-}
+  public static Seed() {
+    if (this.orders.length > 0) return;
+    this.orders.push({
+      id: "1",
+      createdAt: new Date(2024, 7, 21),
+      customer: {
+        name: "Dona Cecilia",
+        notes:
+          "Rua calabreso, 1250 - Centro <br>" +
+          "Forma de pagamento: Dinheiro <br>" +
+          "Troco pra 50 <br>" +
+          "https://maps.com/endereco-do-cara",
+      },
+      description: "Copo de 350 ml com morango e banana",
+      total: 16,
+      totalToRecieve: 10,
+    });
 
-export function Edit(order: Order) {
-  Orders = Orders.map((o) => (o.id === order.id ? order : o));
-  return order;
-}
+    this.orders.push({
+      id: "2",
+      createdAt: new Date(2024, 7, 21),
+      customer: {
+        name: "Cristiano Ronaldo",
+        notes:
+          "Rua santiago Bernabeu, 333 - Bairro Espanha <br>" +
+          "Forma de pagamento: PICSI <br>" +
+          "Troco pra 999999 <br>" +
+          "https://maps.com/endereco-do-cara",
+      },
+      description: "A loja toda",
+      total: 75000,
+      totalToRecieve: 75000,
+    });
+  }
 
-export function Delete(id: string) {
-  Orders = Orders.filter((o) => o.id !== id);
-}
+  public static async Get(id: string): Promise<Order | undefined> {
+    return this.orders.find((o) => o.id === id);
+  }
 
-export function GetAll() {
-  return Orders;
+  public static async Add(order: Order): Promise<Order> {
+    this.orders.push(order);
+    return order;
+  }
+
+  public static async Edit(order: Order): Promise<Order> {
+    this.orders = this.orders.map((o) => (o.id === order.id ? order : o));
+    return order;
+  }
+
+  public static async Delete(id: string): Promise<void> {
+    this.orders = this.orders.filter((o) => o.id !== id);
+  }
+
+  public static async GetAll(): Promise<Order[]> {
+    this.Seed();
+    return this.orders;
+  }
 }
